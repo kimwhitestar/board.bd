ba91e782089b3a63cf74c06080f8d1dab2ab5a7b
kimwhitestar <102268288+kimwhitestar@users.noreply.github.com>
2022-05-12 17:53:40 +0900

    Add files via upload

--- java/board/database/boardreply.sql
+++ java/board/database/boardreply.sql
@@ -10,8 +10,10 @@ create table boardreply (
 	wDate		datetime default now(),			/* 댓글 쓴날짜 */
 	hostIp		varchar(50) not null,			/* client 접속 IP */
 	
-	primary key(idx)							/* 게시판의 기본키 : 고유번호 */
+	primary key(idx),							/* 게시판의 기본키 : 고유번호 */
 	foreign key(boardIdx) references board(idx) /* board테이블의 idx를 boardReply테이블의 외래키로 설정 */
 	on update cascade							/* board테이블의 pk변경에 영향받음 */
 	on delete restrict							/* board테이블의 pk삭제금지 */
 );
+
+desc boardreply;
